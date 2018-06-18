2bc710ce9cfa9bcffc18188d4a952ed80267340e
IslamTareq0012 <31237314+IslamTareq0012@users.noreply.github.com>
2018-06-18 15:36:03 +0200

    change port variable

--- server.js
+++ server.js
@@ -24,7 +24,5 @@ app.use(function (err, req, res, next) {
 });
 
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(process.env.PORT || 3000);
 console.log('on port 3000');
